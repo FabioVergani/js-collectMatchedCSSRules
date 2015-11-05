d1b7e144058e6619b98e622d3c1db03abd6f39b7
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-05 17:23:49 +0100

    Update collectMatchedCSSRules.js

--- collectMatchedCSSRules.js
+++ collectMatchedCSSRules.js
@@ -1,24 +1,15 @@
 void((function($w){
 
- var w=$w, d=w.document, q=[];
-
+ var w=$w, d=w.document, q='pseudo', pe=w[q]||(w[q]=['',':focus',':hover',':active',':link',':visited',':checked',':empty',':disabled',':enabled',':first-child',':first-of-type',':last-child',':last-of-type',':only-of-type',':only-child',':in-range',':out-of-range',':read-only',':read-write',':required',':optional',':valid',':invalid',':target',':root',':after',':before',':first-letter',':first-line','selection']),pL=pe.length+0;
+ q=[];
  for(
-	var o=d.createTreeWalker(d,1,function(){return 1;},true),
-	e=o.currentNode=d.documentElement,
-	f=w.getMatchedCSSRules,
-	g=null;
+	var f=w.getMatchedCSSRules,
+	g=function(a,b){var e=f(a,b);if(e!==null){for(var r=e,m=q,i=0,l=r.length+0;i<l;i++){m[m.length]=r[i];};};},
+	o=d.createTreeWalker(d,1,function(){return 1;},true),
+	e=o.currentNode=d.documentElement;
 	e!==null;
 	e=o.nextNode()
- ){
-	g=f(e,'');
-	if(g!==null){
-		for(var r=g,m=q,i=0,l=r.length;i<l;i++){
-			m[m.length]=r[i];
-      };
-	};
-
- };
-
+ ){for(var i=0,m=pe,l=pL,h=g;i<l;i++){h(e,m[i]);};};
 
 console.log(q);
 
