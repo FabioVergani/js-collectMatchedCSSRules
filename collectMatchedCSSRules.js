2dea078c7b5b48c8d9513f6e7a260b65d94c8b74
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-05 19:32:48 +0100

    Update collectMatchedCSSRules.js

--- collectMatchedCSSRules.js
+++ collectMatchedCSSRules.js
@@ -1,7 +1,15 @@
-//autore:fabio vergani
 void((function($w){
- var w=$w,d=w.document,q='pseudo',k=w[q]||(w[q]=['',':focus',':hover',':active',':link',':visited',':checked',':empty',':disabled',':enabled',':first-child',':first-of-type',':last-child',':last-of-type',':only-of-type',':only-child',':in-range',':out-of-range',':read-only',':read-write',':required',':optional',':valid',':invalid',':target',':root',':after',':before',':first-letter',':first-line','selection']),j=k.length+0;
+ var w=$w,d=w.document,q='mydatas',j=w[q],k=j||(
+	w[q]={
+	 pseudo:['',':focus',':hover',':active',':link',':visited',':checked',':empty',':disabled',':enabled',':first-child',':first-of-type',':last-child',':last-of-type',':only-of-type',':only-child',':in-range',':out-of-range',':read-only',':read-write',':required',':optional',':valid',':invalid',':target',':root',':after',':before',':first-letter',':first-line','selection'],
+	 wop:'<!DOCTYPE html><html><head><meta charset="UTF-8"><title>*Used* cssrules for:&emsp;'+w.location.href+'</title><body style="white-space:pre;font-family:monospace;">'
+	}
+ ),t=k.wop;
+
  q=[];
+ k=k.pseudo;
+ j=k.length+0;
+
  for(
 	var f=w.getMatchedCSSRules,
 	g=function(a,b){
@@ -9,24 +17,22 @@ void((function($w){
 		if(e!==null){
 		for(var s,r=e,m=q,i=0,l=r.length+0;i<l;i++){
 		 s=r[i].cssText;
-		 if(m.indexOf(s)===-1){m[m.length]=s;};
+		 if(m.indexOf(s)===-1){
+			m[m.length]=s;
+		 };
 		};
 	 };
 	},
-	o=d.createTreeWalker(d,1,function(){return 1;},true),
-	e=o.currentNode=d.documentElement;
-	e!==null;
-	e=o.nextNode()
- ){
+ o=d.createTreeWalker(d,1,function(){return 1;},true),e=o.currentNode=d.documentElement;
+ e!==null;
+ e=o.nextNode()){
 	for(var i=0,m=k,l=j,h=g;i<l;i++){h(e,m[i]);};
  };
- 
- q[q.unshift('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>*Used* cssrules for:&emsp;'+w.location.href+'</title><body style="white-space:pre;font-family:monospace;">')]='</body>';
 
+ q[q.unshift(t)]='</body>';
  w=w.open('about:blank','','menubar=no,toolbar=no');
  d=w.document;
-
- d.write(q.join('\n'));
+ t=q.join('\n');
+ d.write(t);
  d.close();
-
 })(window));
