2b81afe18f3914273be8a8ced69100ebf6406032
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-09 11:23:33 +0100

    Update collectMatchedCSSRules.js

--- collectMatchedCSSRules.js
+++ collectMatchedCSSRules.js
@@ -3,10 +3,14 @@ void((function($w){
 	w[q]={
 	 pseudo:['',':focus',':hover',':active',':link',':visited',':checked',':empty',':disabled',':enabled',':first-child',':first-of-type',':last-child',':last-of-type',':only-of-type',':only-child',':in-range',':out-of-range',':read-only',':read-write',':required',':optional',':valid',':invalid',':target',':root',':after',':before',':first-letter',':first-line','selection'],
 	 wop:'<!DOCTYPE html><html><head><meta charset="UTF-8"><title>*Used* cssrules for:&emsp;'+w.location.href+'</title><body style="white-space:pre;font-family:monospace;">',
-	 rgx:{rgb:/rgb(a?)\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*(,\s*(1|0(\.\d+)?)\s*)?\)/gi}
+	 rgx:{
+		 rgb:/rgb(a?)\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*(,\s*(1|0(\.\d+)?)\s*)?\)/gi,
+		 cssunit:/(\d\.)?(\d)\s*(%|px|mm|cm|in|em|rem|en|ex|ch|vm|vw|vh)/gi,
+	 }
 	}
  ),
- rgbx=k.rgx.rgb,
+ cux=(q=k.rgx,q.cssunit),
+ rgbx=q.rgb,
  t=k.wop;
 
  q=[];
@@ -32,34 +36,35 @@ void((function($w){
 	for(var i=0,m=k,l=j,h=g;i<l;i++){h(e,m[i]);};
  };
 
-
  q[q.unshift(t)]='</body>';
- w=w.open('about:blank','','menubar=no,toolbar=no');
- d=w.document;
  t=q.join('\n');
 
  //toHex
- t=t.replace(rgbx,function(s,A,R,G,B,O){
-	var a=A,r=R,g=G,b=B,o=O;
+ t=t.replace(rgbx,function(s,A,R,G,B,_,O,N){
+	var a=A,r=R,g=G,b=B,o=O,n=N;
 	if(a==='a'){
-		o=['rgba('+r,g,b,(o==='0'||o==='1'?o:o.substr(0,4))+')'].join(',');
+		o=(n?n.substr(0,3):o);
+		if(o==='0'){
+			o='transparent';
+		}else{
+			o=['rgba('+r,g,b,o+')'].join(',');
+		};
 	}else{
 		a=function(s){return('0'+parseInt(s).toString(16)).slice(-2);};
 		o=(r===g && g===b);
 		r=a(r);
-		if(o===true){
-			g=r.charAt(1);
-			b=r.charAt(2);
-		}else{
-			g=a(g);
-			b=a(b);
-		};
+		if(o===true){g=r.charAt(1);b=r.charAt(2);}else{g=a(g);b=a(b);};
 		o='#'+r+g+b;
 	};
 	return o;
  });
 
 
- d.write(t);
- d.close();
+
+ if(w=w.open('about:blank','','menubar=no,toolbar=no')){
+  d=w.document;
+  d.write(t);
+  d.close();
+ };
+
 })(window));
